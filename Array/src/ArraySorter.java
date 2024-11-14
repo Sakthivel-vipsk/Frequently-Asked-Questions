dbebf7ebc13bd3659f99b2133fe615ac23aa7727
sakthi <sakthivel.vipsk@gmail.com>
2024-11-14 10:09:50 +0530

    ArraySorter

--- Array/src/ArraySorter.java
+++ Array/src/ArraySorter.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ArraySorter {
@@ -7,7 +8,7 @@ public class ArraySorter {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter size of an array: ");
         int size= sc.nextInt();
-        int[] arr = new int[size];
+        Integer[] arr = new Integer[size];
         System.out.println("Enter the Numbers ");
         for (int i= 0; i<size;i++){
             arr[i] =sc.nextInt();
@@ -15,5 +16,7 @@ public class ArraySorter {
         System.out.println("Before Sorted : " + Arrays.toString(arr));
         Arrays.sort(arr);
         System.out.println("After Sorted : " + Arrays.toString(arr));
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println("Reverse Order : " + Arrays.toString(arr));
     }
 }
